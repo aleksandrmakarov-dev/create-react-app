766d0c53fd7a05f00138475c75a5d58d964d4a9e
aleksandrmakarov-dev <ac5295@student.jamk.fi>
2023-07-04 16:35:56 +0300

    .gitignore

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('test', () => {
+  test('App', () => {});
 });
